aec80885456d717d914c421c0ad1a335b806eca1
DEQepricha <94567240+DEQepricha@users.noreply.github.com>
2023-07-26 17:41:56 -0700

    install dflow from epricha fork of package for dev

---
+++ dflow-dev.R
@@ -0,0 +1,5 @@
+#for dflow test
+#install dflow package from a dev branch
+devtools::install_github("DEQepricha/dflowR-fork@main", 
+                         host = "https://api.github.com", 
+                         dependencies = TRUE, force = TRUE, upgrade = "never")
